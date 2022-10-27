c438c780de4e33ba672b3f43fd29e61881f82db3
dashabezik <dashabezik65@gmail.com>
2022-10-27 18:10:30 +0300

    color function

--- morley/gui.py
+++ morley/gui.py
@@ -6,6 +6,7 @@ import cv2 as cv
 import imutils
 from imutils import contours
 import numpy as np
+import pandas as pd
 
 from functools import partial
 import os
@@ -128,7 +129,7 @@ def blur(param,img_widget, label, morph, gauss,canny_top,event):
             apd = cv.approxPolyDP(cont, 0.02*sm, True)
             cv.drawContours(src, [cont], -1, (255,0,0), -2)
             real_conts.append(cont)
-    print('contours',len(real_conts))
+#     print('contours',len(real_conts))
     state['img_arr'] = src
     src = src.astype('uint8')
     src = imutils.resize(src, height=500)
@@ -137,43 +138,67 @@ def blur(param,img_widget, label, morph, gauss,canny_top,event):
     img_widget['image'] = obj
     group_param = Morley.picture_params()
     Morley.picture_params.contour_params(group_param, morph,gauss,canny_bottom,canny_top)
+    print(group_param.return_bl_params())
     
-def color():
-    morph = state['settings']['morph'].get()
-    morph = 2*morph+1
-    gauss = state['settings']['gauss'].get()
-    gauss = 2*gauss+1
-    canny_bottom = 0
-    canny_top = state['settings']['canny_top'].get()
-    label['text'] = morph*(param==0)+gauss*(param==1)+canny_top*(param==2)
+def color(img_widget, event):
+    src = state['img_color_to_analys']
+    hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV )
+    template = cv.imread(state['paths']['template_file'],0)
+#     template = Morley.rotate_pic(template, rotate)  ### Надо сделать импорт ротейт значения и сделать его вперед ползунков
+    w, h = template.shape[::-1]
     
-    src = state['img_color']
-    gr = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-    bl=cv.GaussianBlur(src,(gauss,gauss),0)
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (morph, morph))
-    closed = cv.morphologyEx(bl, cv.MORPH_CLOSE, kernel)
-    canny = cv.Canny(closed, canny_bottom, canny_top)
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (morph, morph))
-    closed = cv.morphologyEx(canny, cv.MORPH_CLOSE, kernel)
-    contours0 = cv.findContours(closed.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
-    (contours0, _) = contours.sort_contours(contours0)
-    real_conts = []
+    methods = ['cv.TM_CCOEFF_NORMED']
+    for meth in methods:
+        img_color_tmp = cv.imread(state['fname'],0)
+#         img_color_tmp = rotate_pic(img_color_tmp, rotate)
+        method = eval(meth)
+        # Apply template Matching
+        res = cv.matchTemplate(img_color_tmp,template,method)
+        threshold = 0.55
+        loc = np.where( res > threshold)
+        numbers0=[]
+        for pt in zip(*loc[::-1]):
+            if (pt[0] > src.shape[1]/3)&((pt[0] < 2*src.shape[1]/3)):
+                numbers0.append(pt[0])
+    #             cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,0,255), 5)
+
+    numbers = pd.Series(numbers0)
+    Mean = numbers.mean()
+    Median = numbers.median()
+    Mode = numbers.mode()[0]   
+    mean_left_x = int(Mode)-w//4
+    mean_right_x = int(Mode) + 3*w//4
+    mean_left_x = round(mean_left_x)
+    mean_right_x = round(mean_right_x)
+
+    overlay = src.copy()
+    cv.rectangle(overlay, (0,src.shape[0]), (mean_left_x, 0), (0,224,79), -1)
+    opacity = 0.25
+    cv.rectangle(overlay, (mean_right_x, src.shape[0]), (src.shape[1],0), (240,0,255), -5)
+    cv.addWeighted(overlay, opacity, src, 1 - opacity, 0, src)
     
-    for cont in contours0:
-        center, radius = cv.minEnclosingCircle(cont)
-        if ((cv.contourArea(cont)>contour_area_threshold)&
-            (center[0] >src.shape[1]//4)&(center[0] < 2*src.shape[1]//3)):
-            sm = cv.arcLength(cont, True)
-            apd = cv.approxPolyDP(cont, 0.02*sm, True)
-            cv.drawContours(src, [cont], -1, (255,0,0), -2)
-            real_conts.append(cont)
-    state['img_color'] = src
-    print('contours',len(real_conts))
-    src = src.astype('uint8')
-    src = imutils.resize(src, height=500)
-    obj = ImageTk.PhotoImage(Image.fromarray(src))
-    img_widget.image = obj
-    img_widget['image'] = obj
+    h1 = state['color']['h_bottom'].get()
+    s1 = state['color']['s_bottom'].get()
+    v1 = state['color']['v_bottom'].get()
+    h2 = state['color']['h_top'].get()
+    s2 = state['color']['s_top'].get()
+    v2 = state['color']['v_top'].get()
+    
+    # формируем начальный и конечный цвет фильтра
+    h_min = np.array((h1, s1, v1), np.uint8)
+    h_max = np.array((h2, s2, v2), np.uint8)
+
+    # накладываем фильтр на кадр в модели HSV
+    thresh = cv.inRange(hsv, h_min, h_max)
+    
+#     state['img_color_to_analys'] = src
+    thresh = thresh.astype('uint8')
+    thresh = imutils.resize(thresh, height=500)
+    obj_color = ImageTk.PhotoImage(Image.fromarray(thresh))
+    state['img_color_to_analys'] = src
+    state['img_color_to_show'] = thresh
+    img_widget.image = obj_color
+    img_widget['image'] = obj_color
 
 def seed(img_widget, event):
     h1 = state['seed']['h_bottom'].get()
@@ -186,12 +211,14 @@ def seed(img_widget, event):
     h_min = np.array((h1, s1, v1), np.uint8)
     h_max = np.array((h2, s2, v2), np.uint8)
     
-    src = state['img_seed']
+    src = state['img_seed_to_analys']
     hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV )
     thresh = cv.inRange(hsv, h_min, h_max)
     thresh = thresh.astype('uint8')
     thresh = imutils.resize(thresh, height=500)
     obj_color = ImageTk.PhotoImage(Image.fromarray(thresh))
+#     state['img_seed_to_analys'] = src
+    state['img_seed_to_show'] = thresh
     img_widget.image = obj_color
     img_widget['image'] = obj_color
     
@@ -199,7 +226,7 @@ def seed(img_widget, event):
 def rotation(w):
     window = tk.Toplevel(w)
     window.title('Rotate image')
-    window.geometry('300x2  00')
+    window.geometry('300x200')
     var=tk.IntVar()
     var.set(1)
     rad0 = tk.Radiobutton(window, text="90", variable=var, value=0)
@@ -231,8 +258,10 @@ def tweak_image(w):
     img_arr = cv.imread(state['fname'])
     img_arr_2 = imutils.resize(cv.imread(state['fname']), height=500)
     state['img_arr'] = img_arr
-    state['img_color'] = img_arr
-    state['img_seed'] = img_arr
+    state['img_color_to_analys'] = img_arr
+    state['img_color_to_show'] = np.zeros_like(img_arr)
+    state['img_seed_to_analys'] = img_arr
+    state['img_seed_to_show'] = np.zeros_like(img_arr)
     
     def contours_tab():
         img_arr = state['img_arr']
@@ -248,7 +277,7 @@ def tweak_image(w):
         def colors_tab():
             clear(tweak_frame)
             img_frame.destroy()
-            img_arr_color = state['img_color']
+            img_arr_color = state['img_color_to_show']
             img_arr_2_color = imutils.resize((img_arr_color), height=500)
             img_frame_color = tk.Frame(master=window)
             img_obj_color = ImageTk.PhotoImage(Image.fromarray(img_arr_2_color))
@@ -264,7 +293,7 @@ def tweak_image(w):
             for i in sliders_list:
                 l = tk.Label(master=tweak_frame, text=i)
                 s = ttk.Scale(master=tweak_frame, from_ = 0, to = 255, value = state['color'][i].get(),
-                                     variable=state['color'][i])
+                                     variable=state['color'][i], command=partial(color,img_color))
                 s.grid(column=col+1, row=(row//2)*2)
                 l.grid(column=col+1, row=(row//2)*2+1)
                 row = row+1
@@ -273,15 +302,13 @@ def tweak_image(w):
             def back(img_frame_to_destroy, def_to_call):
                 img_frame_to_destroy.destroy()
                 clear(tweak_frame)
-#                 button_b1.destroy()
-#                 button_n2.destroy()
                 def_to_call()
 
             def seeds_tab():
                 button_b1.destroy()
                 button_n2.destroy()
                 img_frame_color.destroy()
-                img_arr_seed = state['img_color']
+                img_arr_seed = state['img_seed_to_show']
                 img_arr_2_seed = imutils.resize((img_arr_seed), height=500)
                 img_frame_seed = tk.Frame(master=window)
                 img_obj_seed = ImageTk.PhotoImage(Image.fromarray(img_arr_2_seed))
@@ -338,6 +365,8 @@ def tweak_image(w):
 
         button_n1 = tk.Button(tweak_frame, text = 'Next', command = colors_tab)
         button_n1.grid(column=6, row=7)
+        
+        
         
         
 
